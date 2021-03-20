02d40b5019604711d735030e7651e9ab37430283
DEFLY <eloifabius@gmail.com>
2021-03-20 21:14:19 +0100

    first commit

---
+++ insta_scraper.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 18:44:46 2021
+
+@author: Kossi Eloi Fabius Defly
+"""
+
+from bs4 import BeautifulSoup
+import json
+import cloudscraper
+import pymongo
+
+post_image = ""
+post_text = ""
+post_comments_url = ""
+post_comments = []
+db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+mycol = db_client["scraping_db"]["insta"]
+
+
+def get_posts(url, post_type):
+    
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    
+    scripts = soup.findAll('script', {'type': 'text/javascript'})[3]
+    clean_data = str(scripts).replace(';</script>', '').replace('<script type="text/javascript">window._sharedData = ', '')
+    
+    json_data = json.loads(clean_data)
+    if post_type == "posts":
+        return json_data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+    else:
+        return json_data['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_parent_comment']['edges']
+
+        
+
+def extract_data(post):
+    post_image = post['node']['display_url']
+    post_text = post['node']['edge_media_to_caption']['edges'][0]['node']['text']
+    post_comments_url = 'https://www.instagram.com/p/' + post['node']['shortcode']
+    
+    comments = get_posts(post_comments_url, "comments")
+    for comment in comments:
+        post_comments.append(comment['node']['text'])
+        
+    print(post_comments_url, post_comments)
+    mydict = { "image": post_image, "text": post_text, "comments": post_comments }
+    insert = mycol.insert_one(mydict)
+        
+
+def connect_bd():
+    db_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mycol = db_client["scraping_db"]["insta"]
+    print("connect to db")
+    
+      
+def main():
+   posts = get_posts("https://www.instagram.com/explore/tags/jacqueschiracdeces/", "posts")
+   
+   for post in posts:
+       extract_data(post)
+       
+if __name__ == "__main__":
+    main()
